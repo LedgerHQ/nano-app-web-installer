2265c3197b50b2263ba7a26e17b2109171da9d86
Thomas Brillard <thomas.brillard@ledger.fr>
2023-02-15 17:01:41 +0100

    npm lib import

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import "./App.css";
 import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
 import { useState } from "react";
-import { getDeviceInfo, installApp, getAppsListByDevice, openApp, quitCurrentApp, getCurrentAppAndVersion } from './lib';
+import { getDeviceInfo, installApp, getAppsListByDevice, openApp, quitCurrentApp, getCurrentAppAndVersion } from 'nano-app-web-installer-lib-test';
 function App() {
   let transport;
   const [error, setError] = useState("");
+  const onClickConnect = async() => {
+    // create connection to nano device
+    transport = await TransportWebUSB.create();
+  }
   const onClickInstall = async (appName, isDelete) => {
     try {
-      transport = await TransportWebUSB.create();
+      // get information about current device
       const deviceInfo = await getDeviceInfo(transport);
-      console.log(deviceInfo);
-      // const version = deviceInfo.version;
+
+      // load all app available for device
       const appByDevice = await getAppsListByDevice(deviceInfo, false, 1);
 
+      // find the correct app for device, should only return one app
       const myApp = appByDevice.filter( app => app.name == appName);
-      console.log(myApp);
 
+      // install the app on the device, need device on dashboard
       await installApp(myApp[0], transport, isDelete);
     } catch (e) {
       setError(String(e));
@@ -24,7 +29,6 @@ function App() {
   
   const onClickOpen = async (appName) => {
     try {
-      transport = await TransportWebUSB.create();
       await openApp(transport, appName);
     } catch (e) {
       setError(String(e));
@@ -32,15 +36,13 @@ function App() {
   }; 
   const onClickQuit = async () => {
     try {
-      transport = await TransportWebUSB.create();
       await quitCurrentApp(transport);
     } catch (e) {
       setError(String(e));
     }
   }; 
-  const onClickGetCurrentAppAndVersion = async () => {
+  const onClickGetAllAppInstalledOnDevice = async () => {
     try {
-      transport = await TransportWebUSB.create();
       const info = await getCurrentAppAndVersion(transport);
       console.log(info);
     } catch (e) {
@@ -50,9 +52,10 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
+        <button onClick={() => onClickConnect()}>Connect nano device</button>
         <button onClick={() => onClickOpen("Cosmos")}>Open app</button>
         <button onClick={() => onClickQuit()}>Quit app</button>
-        <button onClick={() => onClickGetCurrentAppAndVersion()}>Get info app</button>
+        <button onClick={() => onClickGetAllAppInstalledOnDevice()}>Get info app</button>
         <button onClick={() => onClickInstall("Cosmos")}>Install app</button>
         <button onClick={() => onClickInstall("Cosmos", true)}>
           Remove app
