b74007038cd1d7d6e169a4832e7f50a72ccf9ec8
Thomas Brillard <thomas.brillard@ledger.fr>
2023-02-15 11:07:59 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import "./App.css";
 import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
-import TransportWebBLE from "@ledgerhq/hw-transport-web-ble";
 import { appsList } from "./api";
 import { useState } from "react";
-import { getDeviceInfo, getTargetId, installApp, getAppsListByDevice } from './lib';
+import { getDeviceInfo, getTargetId, installApp, getAppsListByDevice, getAppsList } from './lib';
 function App() {
   let transport;
   const [error, setError] = useState("");
   const onClickInstall = async (appName, isDelete) => {
     try {
       transport = await TransportWebUSB.create();
-
+      const device = transport.deviceModel.id.toLowerCase();
       const apps = await appsList();
 
       const family = apps.find(
@@ -20,56 +19,29 @@ function App() {
 
       console.log("transport");
       console.log(transport);
-      const lol = await getDeviceInfo(transport);
-      console.log(lol);
+      const lol = await getAppsList();
+      const deviceInfo = await getDeviceInfo(transport);
+      console.log(deviceInfo);
+      // const version = deviceInfo.version;
+      const appByDevice = await getAppsListByDevice(deviceInfo, false);
+      console.log(appByDevice);
 
       // TODO get device version
       const app = family.application_versions.find(
-        (a) => a.firmware === "nanox/2.0.2-2/ATOM/app_2.34.9"
+        (a) => a.firmware === `${device}/2.0.2-2/ATOM/app_2.34.9`
       );
 
-      const targetId = await getTargetId(transport);
+      // const targetId = await getTargetId(transport);
 
-      await installApp(targetId, app, transport, isDelete);
+      // await installApp(app, transport, isDelete);
     } catch (e) {
       setError(String(e));
     }
-  };  
-  const onClickInstallBLE = async (appName, isDelete) => {
-    try {
-      transport = await TransportWebBLE.create();
-
-      const apps = await appsList();
-
-      const family = apps.find(
-        (a) =>
-          a.name.toLowerCase() === appName.toLowerCase() && a.category !== 2
-      );
-
-      console.log("transport");
-      console.log(transport);
-      const lol = await getDeviceInfo(transport);
-      console.log(lol);
-      // const lolilol = await getAppsListByDevice(lol, false);
-      // console.log(lolilol);
-
-      // TODO get device version
-      const app = family.application_versions.find(
-        (a) => a.firmware === "nanox/2.0.2-2/ATOM/app_2.34.9"
-      );
-
-      const targetId = await getTargetId(transport);
-
-      await installApp(targetId, app, transport, isDelete);
-    } catch (e) {
-      setError(String(e));
-    }
-  };
+  }; 
   return (
     <div className="App">
       <div className="App-header">
         <button onClick={() => onClickInstall("Cosmos")}>Install app</button>
-        <button onClick={() => onClickInstallBLE("Cosmos")}>Install app Bluetooth</button>
         <button onClick={() => onClickInstall("Cosmos", true)}>
           Remove app
         </button>
